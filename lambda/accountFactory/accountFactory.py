601c0aec19e797548181220628691fd820d11f18
mrpackethead <andrew.frazer@raindancers.cloud>
2023-09-27 10:29:15 +0000

    fix: add delete and update to lambda

--- lambda/accountFactory/accountFactory.py
+++ lambda/accountFactory/accountFactory.py
@@ -5,6 +5,27 @@ servicecatalog = boto3.client('servicecatalog')
 orgs = boto3.client('organizations')
 
 def on_event(event, context):
+  print(event)
+  request_type = event['RequestType']
+  if request_type == 'Create': return on_create(event)
+  if request_type == 'Update': return on_update(event)
+  if request_type == 'Delete': return on_delete(event)
+  raise Exception("Invalid request type: %s" % request_type)
+
+def on_update(event):
+	print('immutable resource, no changes')
+
+def on_delete(event):
+
+	print('deleting resource')
+	response = servicecatalog.terminate_provisioned_product(
+		ProvisionedProductName=f"awsAccount-{props['ProvisionedProductName']}",
+		ProvisionedProductId=event['PhysicalResourceId'],
+		IgnoreErrors=True
+	)
+
+
+def on_create(event):
 
 	props = event["ResourceProperties"]
 	provisioning_artifact_id = servicecatalog.describe_product(
@@ -28,47 +49,60 @@ def on_event(event, context):
 	}
 
 def is_complete(event, context):
-
-	props = event["ResourceProperties"]
-	provisioning_parameters = json.loads(props["ProvisioningParameters"])
-	print(provisioning_parameters)
+	print(event)
+	request_type = event['RequestType']
 	
 
-	# find the provisioned product
-	state = servicecatalog.search_provisioned_products(
-		Filters={
-			'SearchQuery': [f"id:{event['PhysicalResourceId']}"]
-		},
-	)
-
-	print(state['ProvisionedProducts'][0]['Status'])
-
-	if state['ProvisionedProducts'][0]['Status'] == 'ERROR':
-		raise Exception(f"ProvisionedProduct has failed to provision")
-
-
-	# if its AVAIALABLE, its complete.
-	if state['ProvisionedProducts'][0]['Status'] in ['AVAILABLE', 'TAINTED']:
-
-		allaccounts = []
-		paginator = orgs.get_paginator('list_accounts')
-		getAccounts = paginator.paginate()
+	if request_type == 'Create':
+  
+		props = event["ResourceProperties"]
+		provisioning_parameters = json.loads(props["ProvisioningParameters"])
+		print(provisioning_parameters)
 		
-		
-		for page in getAccounts:
-			for account in page['Accounts']:
-				if account["Email"] == provisioning_parameters["AccountEmail"]:
-					print(account["Email"])
-					return { 
-						'Data': { 
-							'AccountId': account['Id'],
-							'Arn': account['Arn'],
-							'Name': account['Name'] 
-						},
-						'IsComplete': True 
-					}
 
-		raise Exception(f"Account {props['ProvisioningParameters']['AccountEmail']} not found")
+		# find the provisioned product
+		state = servicecatalog.search_provisioned_products(
+			Filters={
+				'SearchQuery': [f"id:{event['PhysicalResourceId']}"]
+			},
+		)
 
-	else:
-		return { 'IsComplete': False }
+		print(state['ProvisionedProducts'][0]['Status'])
+
+		if state['ProvisionedProducts'][0]['Status'] == 'ERROR':
+			raise Exception(f"ProvisionedProduct has failed to provision")
+
+
+		# if its AVAIALABLE, its complete.
+		if state['ProvisionedProducts'][0]['Status'] in ['AVAILABLE', 'TAINTED']:
+
+			allaccounts = []
+			paginator = orgs.get_paginator('list_accounts')
+			getAccounts = paginator.paginate()
+			
+			
+			for page in getAccounts:
+				for account in page['Accounts']:
+					if account["Email"] == provisioning_parameters["AccountEmail"]:
+						print(account["Email"])
+						return { 
+							'Data': { 
+								'AccountId': account['Id'],
+								'Arn': account['Arn'],
+								'Name': account['Name'] 
+							},
+							'IsComplete': True 
+						}
+
+			raise Exception(f"Account {props['ProvisioningParameters']['AccountEmail']} not found")
+
+		else:
+			
+
+	if request_type == 'Update': 
+		return { 'IsComplete': True }
+
+	if request_type == 'Delete': 
+		return { 'IsComplete': True }
+	
+	raise Exception("Invalid request type: %s" % request_type)
