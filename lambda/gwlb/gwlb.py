f31098c9c0e2535cb441c5849ec21a14c27c7095
Andrew <andrew.frazer@raindancers.cloud>
2025-06-24 01:50:55 +0000

    fix: endpointId was wrong

--- lambda/gwlb/gwlb.py
+++ lambda/gwlb/gwlb.py
@@ -39,7 +39,7 @@ def handler(event, context):
             return {
                 'PhysicalResourceId': props['Name'],
                 'Data': {
-                    'Endpoint': endpoint['VpcEndpointId']
+                    'EndpointId': endpoint['VpcEndpointId']
                 }
             }
 
