0702d565c2a7009f7e81e29b7c0c30d41aa2b9c2
Andrew <andrew.frazer@raindancers.cloud>
2025-07-22 08:05:40 +0000

    fix: add print to lamba

--- lambda/cloudNetwork/lambda/ipam/bigwait.py
+++ lambda/cloudNetwork/lambda/ipam/bigwait.py
@@ -1,4 +1,4 @@
-import boto3
+#import boto3
 import json
 import time
 from datetime import datetime, timezone
@@ -23,20 +23,41 @@ def handler(event, context):
         # Parse the ISO string to datetime
         start_time = datetime.fromisoformat(start_time_str.replace('Z', '+00:00'))
         current_time = datetime.now(timezone.utc)
-        
-        # Calculate end time
         end_time = start_time.timestamp() + timer_duration
         current_timestamp = current_time.timestamp()
+
+        print('start_time', start_time)
+        print('current_time', current_timestamp)
+        print('end_time', end_time)
+
+        # Calculate end time
+        
         
         if current_timestamp >= end_time:
+            print('wait is over')
             return {
                 'PhysicalResourceId': 'bigwait',
                 'IsComplete': True
             }
         else:
+            print('wait some more')
             return {
                 'PhysicalResourceId': 'bigwait',
                 'IsComplete': False
             }
     
     raise Exception("Invalid RequestType")
+
+
+# start_time = '2025-07-22T07:50:19.829Z'
+# timer_duration = 400
+
+# print(handler({
+#     'RequestType': 'Create',
+#     'ResourceProperties': {
+#         'StartTimer': start_time,
+#         'TimerDuration': timer_duration
+#     }
+# },{}
+# ))
+
