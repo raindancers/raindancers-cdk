c76d373ca868fc48cd65206c8163f6c7df72561f
Andrew <andrew.frazer@raindancers.cloud>
2025-07-30 22:24:11 +0000

    fix: removed totalTimeout

--- src/cloudNetwork/inspectionRoutes.ts
+++ src/cloudNetwork/inspectionRoutes.ts
@@ -60,7 +60,6 @@ export class InspectionRoutes extends constructs.Construct {
 
     const provider = new cr.Provider(this, 'poolCidrWaiterProvider', {
       onEventHandler: routeExistsFn,
-      totalTimeout: core.Duration.minutes(30),
     });
 
 
